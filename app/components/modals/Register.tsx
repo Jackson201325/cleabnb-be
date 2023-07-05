23581fb5c81769eb719bc8eda3351c05217ec167
Jackson Huang <jacksonh201325@gmail.com>
2023-07-05 09:14:58 -0400

    feat: adds register form render logic

--- app/components/modals/Register.tsx
+++ app/components/modals/Register.tsx
@@ -6,6 +6,9 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 import Modal from './Modal'
+import Heading from '../Heading'
+import Input from '../inputs/Input'
+import Button from '../Button'
 
 type Props = {}
 
@@ -33,14 +36,70 @@ const Register: React.FC<Props> = (props: Props) => {
     setIsLoading(false)
   }
 
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an account" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        errors={errors}
+        register={register}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        errors={errors}
+        register={register}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        errors={errors}
+        register={register}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Sign In With Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Sign In With Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>Already have an account?</div>
+          <div onClick={registerModal.close} className="text-neutral-800 cursor-pointer hover:underline"></div>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <Modal
-      disabled={isLoading}
-      isOpen={registerModal.isOpen}
-      title="Register"
       actionLabel="Continue"
+      body={bodyContent}
+      disabled={isLoading}
+      footer={footerContent}
+      isOpen={registerModal.isOpen}
       onClose={registerModal.close}
       onSubmit={handleSubmit(onSubmit)}
+      title="Register"
     />
   )
 }
